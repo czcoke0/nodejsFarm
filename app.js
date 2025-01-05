07c72dcb01b1fe7f9292e892c828f04f55455cbb
cz <clairezhangxy@gmail.com>
2025-01-05 17:54:09 +0800

    add middleware tourRouter & userRouter

--- app.js
+++ app.js
@@ -151,20 +151,16 @@ const deleteUser = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
+tourRouter.route('/').get(getAllTours).post(createTour); // the route is the root
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app.route('/app/v1/users').get(getAllUsers).post(createUser);
-app
-  .route('/app/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
+app.use('/api/v1/tours', tourRouter); //middleware, only run for the route with /api/v1/tours
+app.use('/api/v1/users', userRouter);
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
