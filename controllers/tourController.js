8dddf7977d0e4db4bb250323ff7cd1f4a831161b
cz <clairezhangxy@gmail.com>
2025-01-14 22:58:24 +0800

    aliasing api /top-5-cheap

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,12 @@
 //handlers are controller functions
 const Tour = require('../models/tourModel');
 
+const getTopTours = async (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 const getAllTours = async (req, res) => {
   try {
     console.log(req.query);
@@ -136,10 +142,12 @@ const deleteTour = async (req, res) => {
     });
   }
 };
+
 module.exports = {
   getAllTours,
   getTour,
   createTour,
   updateTour,
   deleteTour,
+  getTopTours,
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,13 +5,14 @@ const {
   createTour,
   updateTour,
   deleteTour,
+  getTopTours,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id');
 //this is a middleware that only runs for the route with /api/v1/tours, not users!
-
+router.route('/top-5-cheap').get(getTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour); // the route is the root
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
