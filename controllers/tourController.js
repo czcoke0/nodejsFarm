d0e75ebc516c27c2c6bd223a72440e8d42d5347e
cz <clairezhangxy@gmail.com>
2025-01-16 23:51:21 +0800

    pipeline_unwind

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -139,6 +139,57 @@ const getTourStats = async (req, res) => {
   }
 };
 
+const getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1; //2025, transform to number by multiplying by 1
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates', //$unwind is the name of the stage
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 6,
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
 module.exports = {
   getAllTours,
   getTour,
@@ -147,4 +198,5 @@ module.exports = {
   deleteTour,
   getTopTours,
   getTourStats,
+  getMonthlyPlan,
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const {
   deleteTour,
   getTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
@@ -14,6 +15,7 @@ const router = express.Router();
 // router.param('id');
 //this is a middleware that only runs for the route with /api/v1/tours, not users!
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-cheap').get(getTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour); // the route is the root
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
