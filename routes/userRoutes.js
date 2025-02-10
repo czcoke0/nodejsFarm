72ab8f36a512690145308c83d24bf35ce0f13907
cz <clairezhangxy@gmail.com>
2025-02-10 22:59:11 +0800

    auth_signup route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,10 +7,12 @@ const {
   deleteUser,
   checkUser,
 } = require('../controllers/userController');
-
+const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.param('id', checkUser);
+
+router.post('/signup', authController.signup); //signup is specific to authController, not related to userController endpoints. signup only needs post method
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
